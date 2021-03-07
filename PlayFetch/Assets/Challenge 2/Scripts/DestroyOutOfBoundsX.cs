617fe5e0ec8eb06a437940efe9ac0553d33054e9
Aaro <aaronmoyaarques@gmail.com>
2021-03-07 22:03:48 +0100

    PlayFetch added

---
+++ PlayFetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30f;
+    private float bottomLimit = 0.23f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (this.gameObject.CompareTag("Balls") && transform.position.y < bottomLimit)
+        {
+            Debug.Log("Game Over...");
+            Time.timeScale = 0f;
+        }
+
+    }
+}
---
+++ PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float counter = 2f;
+    public float waitTime = 2f;
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (counter >= waitTime)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                counter = 0f;
+            }
+        }
+    }
+}
