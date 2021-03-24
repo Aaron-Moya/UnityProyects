47bf68ba6e7dc8c70d4d564236a672334db63a2b
Aaro <aaronmoyaarques@gmail.com>
2021-03-24 16:26:50 +0100

    SumoBalls added

---
+++ SumoBalls/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float moveForce;
+
+    public GameObject focalPoint;
+
+    private float forwardInput;
+
+    public bool hasPoweUp;
+    public float powerUpForce;
+    public float powerUpCoolDown;
+    public GameObject[] powerUpIndicators;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        _rigidbody.AddForce(focalPoint.transform.forward * (moveForce * forwardInput));
+
+        foreach (GameObject indicator in powerUpIndicators)
+        {
+            indicator.transform.position = this.transform.position;  // + 0.5f * Vector3.down;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPoweUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDown());
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && hasPoweUp)
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - this.transform.position;
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpForce, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountDown()
+    {
+        foreach (GameObject indicator in powerUpIndicators)
+        {
+            indicator.gameObject.SetActive(true);
+            yield return new WaitForSeconds(powerUpCoolDown/powerUpIndicators.Length);
+            indicator.gameObject.SetActive(false);  
+        }
+        
+        hasPoweUp = false;
+    }
+}
---
+++ SumoBalls/Assets/_Scripts/SpawnManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float randomRange = 9;
+
+    public int enemyCount;
+    public int enemyWave = 1;
+
+    public GameObject powerUpPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(enemyWave);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if (enemyCount == 0)
+        {
+            enemyWave++;
+            SpawnEnemyWave(enemyWave);
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(randomRange, -randomRange);
+        float spawnPosZ = Random.Range(randomRange, -randomRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+
+    private void SpawnEnemyWave(int numberOfEnemies)
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+}
